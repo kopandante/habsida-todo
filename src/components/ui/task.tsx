9924923d6a0975d70310f38b1ff0ea4a76e3c322
kopandroid <panda@kopanda.ru>
2024-07-18 16:22:49 +0900

    Adding Interactivity

--- src/components/ui/task.tsx
+++ src/components/ui/task.tsx
@@ -1,8 +1,14 @@
+import { useCallback, useState } from "react";
+
 interface TaskProps {
   completed?: boolean;
   editing?: boolean;
   description: string;
   created: Date;
+  onComplete: () => void;
+  onDelete: () => void;
+  onPressEdit: () => void;
+  onEdit: (value: string) => void;
 }
 
 const Task: React.FC<TaskProps> = ({
@@ -10,8 +16,12 @@ const Task: React.FC<TaskProps> = ({
   editing = false,
   description,
   created,
+  onComplete,
+  onDelete,
+  onPressEdit,
+  onEdit,
 }) => {
-  const TimeOffset = (date: Date) => {
+  const TimeOffset = useCallback((date: Date) => {
     const now = new Date();
     const second = 1000;
     const minute = 60 * second;
@@ -30,22 +40,44 @@ const Task: React.FC<TaskProps> = ({
     } else if (offset >= day) {
       return `created ${~~(offset / day)} days ago`;
     }
+  }, []);
+
+  const [editValue, setEditValue] = useState(description);
+
+  const handleEditChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEditValue(event.target.value);
   };
 
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      onEdit(editValue);
+    }
+  };
   return (
     <li
       className={`${completed ? "completed" : ""} ${editing ? "editing" : ""}`}
     >
       <div className="view">
-        <input className="toggle" type="checkbox" />
+        <input
+          className="toggle"
+          type="checkbox"
+          checked={completed}
+          onClick={onComplete}
+        />
         <label>
           <span className="description">{description}</span>
           <span className="created">{TimeOffset(created)}</span>
         </label>
-        <button className="icon icon-edit"></button>
-        <button className="icon icon-destroy"></button>
+        <button className="icon icon-edit" onClick={onPressEdit}></button>
+        <button className="icon icon-destroy" onClick={onDelete}></button>
       </div>
-      {editing && <input type="text" className="edit" value={description} />}
+      <input
+        type="text"
+        className={`${!editing && "hidden"} edit`}
+        value={editValue}
+        onChangeCapture={handleEditChange}
+        onKeyDown={handleKeyDown}
+      />
     </li>
   );
 };
--- src/ui/tasklist.tsx
+++ src/ui/tasklist.tsx
@@ -1,38 +1,74 @@
+"use client";
+
+import { useState } from "react";
 import Task from "@/components/ui/task";
 
 export default function TaskList() {
   const now = new Date();
 
-  const data = [
+  const [tasks, setTasks] = useState([
     {
+      id: 1,
       completed: true,
       editing: false,
       description: "Completed task",
       created: new Date(now.getTime() - 17 * 1000),
     },
     {
+      id: 2,
       completed: false,
       editing: true,
       description: "Editing task",
       created: new Date(now.getTime() - 5 * 60 * 1000),
     },
     {
+      id: 3,
       completed: false,
       editing: false,
       description: "Active task",
       created: new Date(now.getTime() - 5 * 60 * 1000),
     },
-  ];
+  ]);
+
+  const handleComplete = (id: number) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, completed: !task.completed } : task
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const handlePressEdit = (id: number) => {
+    setTasks(
+      tasks.map((task) => (task.id === id ? { ...task, editing: true } : task))
+    );
+  };
+
+  const handleEdit = (id: number, value: string) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, description: value, editing: false } : task
+      )
+    );
+  };
 
   return (
     <ul className="todo-list">
-      {data.map((item, index) => (
+      {tasks.map((task) => (
         <Task
-          key={index}
-          completed={item.completed}
-          editing={item.editing}
-          description={item.description}
-          created={item.created}
+          key={task.id}
+          completed={task.completed}
+          editing={task.editing}
+          description={task.description}
+          created={task.created}
+          onComplete={() => handleComplete(task.id)}
+          onDelete={() => handleDelete(task.id)}
+          onPressEdit={() => handlePressEdit(task.id)}
+          onEdit={(value) => handleEdit(task.id, value)}
         />
       ))}
     </ul>
